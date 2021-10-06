3e3bb0d801ddeebec4aef82a7bde2f783c349a68
Travis <travis.goddaer@bigpond.com>
2021-10-06 11:53:59 +1000

    06/10/21

---
+++ app/src/main/java/travisgoddaer/com/backtobasics/MainActivity.java
@@ -0,0 +1,28 @@
+package travisgoddaer.com.backtobasics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void onClick(View view) {
+        toastMsg("Going to page 2");
+        startActivity(new Intent(MainActivity.this,MainActivity2.class));
+    }
+
+}
